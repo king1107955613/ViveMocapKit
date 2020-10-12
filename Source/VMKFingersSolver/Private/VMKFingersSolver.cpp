afeaf203f0f632d0ca79553ee43a43aa34fbff87
FisherTicker <46066864+FisherTicker@users.noreply.github.com>
2020-10-12 15:47:20 +0800

    1
    更换

--- Source/VMKFingersSolver/Private/VMKFingersSolver.cpp
+++
@@ -1,7 +0,0 @@
-// VR IK Body Component
-// (c) Yuri N Kalinin, 2017, ykasczc@gmail.com. All right reserved.
-
-#include "VMKFingersSolver.h"
-#include "Modules/ModuleManager.h"
- 
-IMPLEMENT_MODULE(FVMKFingersSolver, VMKFingersSolver)
--- Source/ViveMocapKit/Private/FingersDataProvider.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Yuri N K, 2018. All Rights Reserved.
-// Support: ykasczc@gmail.com
-
-#include "FingersDataProvider.h"
-#include "CaptureDevice.h"
-#include "VMKFingersSolverSetup.h"
-#include "VMKFingersFKIKSolver.h"
